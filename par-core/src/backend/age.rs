70985370e95b53f58e3e4d2ecd4d3383257f089d
hepptho <hepptho-github.sbd2s@simplelogin.com>
2024-07-23 17:58:15 +0200

    core: refactor age identity handling

--- par-core/src/backend/age.rs
+++ par-core/src/backend/age.rs
@@ -5,9 +5,8 @@ use std::io::Read;
 use std::path::Path;
 use std::process::{Command, Stdio};
 
-use crate::age::identity::{identity_file, read_identity_or_get_cached};
+use crate::age::identity::get_identity;
 use crate::backend::{exit_status_to_result, output_to_result};
-use anyhow::anyhow;
 
 pub const RECIPIENT_FILE_NAME: &str = ".age-id";
 
@@ -29,30 +28,25 @@ pub fn encrypt(content: &mut impl Read, recipient: &str, out_path: &Path) -> any
 }
 
 pub fn decrypt(path: &Path) -> anyhow::Result<String> {
-    let identity_from_stdin = read_identity_or_get_cached()?;
-
-    let cmd_identity = if identity_from_stdin.is_some() {
-        "-".to_string()
-    } else {
-        identity_file().ok_or(anyhow!("No age identity provided"))?
-    };
+    let identity = get_identity()?;
     let mut child = Command::new("age")
         .arg("--decrypt")
         .arg("--identity")
-        .arg(cmd_identity)
+        .arg("-")
         .arg(path.as_os_str())
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?;
+
     let mut child_stdin = child.stdin.take().unwrap();
-    let content = if let Some(identity) = identity_from_stdin {
-        identity.clone()
-    } else {
-        vec![]
-    };
-    io::copy(&mut content.as_slice(), &mut child_stdin)?;
+    io::copy(&mut identity.as_slice(), &mut child_stdin)?;
     drop(child_stdin);
     let output = child.wait_with_output()?;
     output_to_result(output)
+}
+
+pub fn initialize_identity() -> anyhow::Result<()> {
+    get_identity()?;
+    Ok(())
 }
--- par-core/src/backend/age/identity.rs
+++ par-core/src/backend/age/identity.rs
@@ -1,29 +1,57 @@
+use crate::backend::is_age_encrypted;
+use anyhow::anyhow;
 use std::io::{IsTerminal, Read};
+use std::path::Path;
+use std::process::{Command, Stdio};
 use std::sync::OnceLock;
-use std::{env, io};
+use std::{env, fs, io};
 
-static IDENTITY: OnceLock<Option<Vec<u8>>> = OnceLock::new();
+static IDENTITY: OnceLock<anyhow::Result<Vec<u8>>> = OnceLock::new();
 
-fn read_identity_from_stdin() -> anyhow::Result<Option<Vec<u8>>> {
-    if io::stdin().is_terminal() {
-        Ok(None)
+pub fn get_identity() -> anyhow::Result<&'static Vec<u8>> {
+    IDENTITY
+        .get_or_init(|| {
+            if !io::stdin().is_terminal() {
+                read_identity_from_stdin()
+            } else if let Some(identity_file) = identity_file() {
+                read_identity_from_file(identity_file)
+            } else {
+                Err(anyhow!(""))
+            }
+        })
+        .as_ref()
+        .map_err(|err| anyhow!(err))
+}
+
+fn read_identity_from_stdin() -> anyhow::Result<Vec<u8>> {
+    let mut identity = Vec::new();
+    io::stdin().read_to_end(&mut identity)?;
+    Ok(identity)
+}
+
+fn read_identity_from_file(identity_file: String) -> anyhow::Result<Vec<u8>> {
+    if is_age_encrypted(Path::new(identity_file.as_str()))? {
+        decrypt_identity_file(identity_file)
     } else {
-        let mut identity = Vec::new();
-        io::stdin().read_to_end(&mut identity)?;
-        Ok(Some(identity))
+        fs::read(identity_file).map_err(anyhow::Error::from)
     }
 }
 
-pub fn read_identity_or_get_cached() -> anyhow::Result<Option<&'static Vec<u8>>> {
-    let identity_from_stdin = if let Some(id) = IDENTITY.get() {
-        id
+fn decrypt_identity_file(identity_file: String) -> anyhow::Result<Vec<u8>> {
+    let output = Command::new("age")
+        .arg("--decrypt")
+        .arg(identity_file)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .output()?;
+    if output.status.success() {
+        Ok(output.stdout)
     } else {
-        let id = read_identity_from_stdin()?;
-        IDENTITY.get_or_init(|| id)
-    };
-    Ok(identity_from_stdin.as_ref())
+        Err(anyhow!("age exited with an error"))
+    }
 }
 
-pub fn identity_file() -> Option<String> {
+fn identity_file() -> Option<String> {
     env::var("AGE_IDENTITY").ok()
 }
