e69844f3d04934c166c1b17219a37664bf481065
hepptho <hepptho-github.sbd2s@simplelogin.com>
2024-08-08 17:28:40 +0200

    cli: refactor animation

--- par-cli/src/command/generate/animation.rs
+++ par-cli/src/command/generate/animation.rs
@@ -1,44 +1,38 @@
 use crossterm::style::Stylize;
 use par_core;
 use rand::prelude::SliceRandom;
+use std::cmp::min;
 use std::io::Write;
 use std::time::Duration;
 use std::{io, thread};
 
-const PLACEHOLDER_CHAR: u8 = b'\0';
 pub(crate) fn animate(pass: &str) {
     let len = pass.len();
-    let mut state = vec![PLACEHOLDER_CHAR; len];
+    let mut state: Vec<Option<char>> = vec![None; len];
     while let Some(&i) = missing_indexes(&state).choose(&mut rand::thread_rng()) {
         advance_and_print_state(pass, &mut state, i);
     }
 }
 
-fn advance_and_print_state(pass: &str, state: &mut [u8], i: usize) {
-    state[i] = pass.as_bytes()[i];
+fn advance_and_print_state(pass: &str, state: &mut [Option<char>], i: usize) {
+    state[i] = pass.chars().nth(i);
     let s = state.iter().fold(String::new(), |mut acc, &elem| {
-        if elem == PLACEHOLDER_CHAR {
-            acc.push(par_core::random_ascii());
+        if let Some(char) = elem {
+            acc.push_str(format!("{}", char.bold()).as_str());
         } else {
-            acc.push_str(format!("{}", (elem as char).bold()).as_str());
+            acc.push(par_core::random_ascii());
         }
         acc
     });
     print!("\r{s}");
     io::stdout().flush().unwrap();
-    thread::sleep(Duration::from_millis((2000 / pass.len()) as u64));
+    thread::sleep(Duration::from_millis(min(50, 2000 / pass.len()) as u64));
 }
 
-fn missing_indexes(state: &[u8]) -> Vec<usize> {
+fn missing_indexes(state: &[Option<char>]) -> Vec<usize> {
     state
         .iter()
         .enumerate()
-        .filter_map(|(i, c)| {
-            if *c == PLACEHOLDER_CHAR {
-                Some(i)
-            } else {
-                None
-            }
-        })
+        .filter_map(|(i, c)| if c.is_none() { Some(i) } else { None })
         .collect()
 }
