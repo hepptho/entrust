92f5a07d935f5d4638a3dc5281f27bfa3c0f2bfe
hepptho <hepptho-github.sbd2s@simplelogin.com>
2024-08-11 13:55:15 +0200

    cli: move tracing config to module

--- par-cli/src/bin/par.rs
+++ par-cli/src/bin/par.rs
@@ -1,10 +1,14 @@
+#[cfg(feature = "tracing")]
+#[path = "../tracing.rs"]
+mod tracing;
+
+#[cfg(feature = "tracing")]
+use crate::tracing::init_tracing;
 use clap::Parser;
 use par_cli::alias::apply_aliases;
 use par_cli::command;
 use par_cli::command::ParArgs;
 use std::env;
-#[cfg(feature = "tracing")]
-use {std::env, std::fs::OpenOptions, std::sync::Arc, tracing_subscriber::EnvFilter};
 
 fn main() -> anyhow::Result<()> {
     #[cfg(feature = "tracing")]
@@ -12,21 +16,4 @@ fn main() -> anyhow::Result<()> {
     let mut args: Vec<_> = env::args().collect();
     apply_aliases(&mut args);
     command::run(ParArgs::parse_from(args))
-}
-
-#[cfg(feature = "tracing")]
-fn init_tracing() -> anyhow::Result<()> {
-    if let Ok(log_file) = env::var("PAR_LOG_FILE") {
-        let log_file = OpenOptions::new()
-            .create(true)
-            .append(true)
-            .open(log_file)?;
-        tracing_subscriber::fmt()
-            .pretty()
-            .with_env_filter(EnvFilter::from_default_env())
-            .with_writer(Arc::new(log_file))
-            .with_ansi(false)
-            .init();
-    }
-    Ok(())
 }
