4b80daef029c59d6e1b895b1ec852b09a9051421
hepptho <hepptho-github.sbd2s@simplelogin.com>
2025-02-26 17:03:05 +0100

    cli: add nushell completions

--- entrust/src/command/completions.rs
+++ entrust/src/command/completions.rs
@@ -1,17 +1,25 @@
 use crate::command::EntArgs;
 use clap::{Args, CommandFactory};
 use clap_complete::{generate, Shell};
+use clap_complete_nushell::Nushell;
 use std::io;
 
 pub(super) const ABOUT: &str = "Generate shell completions";
 
 #[derive(Args, Debug)]
 pub struct CompletionsArgs {
-    shell: Shell,
+    shell: Option<Shell>,
+    #[arg(short, long, conflicts_with = "shell")]
+    nushell: bool,
 }
 
 pub fn run(args: CompletionsArgs) {
     let mut cmd = EntArgs::command();
     let bin_name = cmd.get_bin_name().unwrap_or(cmd.get_name()).to_string();
-    generate(args.shell, &mut cmd, bin_name, &mut io::stdout());
+    if let Some(shell) = args.shell {
+        generate(shell, &mut cmd, &bin_name, &mut io::stdout());
+    }
+    if args.nushell {
+        generate(Nushell, &mut cmd, &bin_name, &mut io::stdout());
+    }
 }
